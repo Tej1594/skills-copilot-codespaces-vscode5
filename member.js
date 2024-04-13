54afc51d59671f6ea6a7ccb65b3c0347c25762c3
Tej1594 <166800452+Tej1594@users.noreply.github.com>
2024-04-13 06:43:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMembers() {
+  var member = {
+    name: 'John',
+    age: 20,
+    skills: ['HTML', 'CSS', 'JS'],
+    address: {
+      city: 'New York',
+      country: 'USA'
+    }
+  };
+  return member;
+}
